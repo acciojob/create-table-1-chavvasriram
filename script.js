41c15ab8f6903f3b8d8b4e07841d14e5f0d2a0b8
chavvasriram <chavvasriram@gmail.com>
2025-09-16 16:00:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function insert_Row() {
     //Write your code here
-  
+            const table=document.getElementById("sampleTable")
+            const tr=document.createElement("tr")
+            const td1=document.createElement("td")
+            td1.innerText=`NewCell1`
+            const td2=document.createElement("td")
+            td2.innerText=`NewCell2`
+            tr.append(td1,td2)
+            const firstchild=table.firstChild
+            table.insertBefore(tr,firstchild)
+            console.log("row added")
   
 }
