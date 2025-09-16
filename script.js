aca2a6caca728d542ef547824e441e619cd0f319
chavvasriram <chavvasriram@gmail.com>
2025-09-16 16:09:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-let count=0
+
 function insert_Row() {
     //Write your code here
-	count++
+	
             const table=document.getElementById("sampleTable")
             const tr=document.createElement("tr")
             const td1=document.createElement("td")
-            td1.innerText=`NewCell1${count++}`
+            td1.innerText=`New Cell1`
             const td2=document.createElement("td")
-            td2.innerText=`NewCell${count}`
+            td2.innerText=`New Cell2`
             tr.append(td1,td2)
             const firstchild=table.firstChild
             table.insertBefore(tr,firstchild)
